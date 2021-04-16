9caf0a6320116aaddddac645d636975784c406ef
Oliver Shih <olivershih@gmail.com>
2021-04-15 22:06:37 -0700

    delete buttons work properly, fixed the bug that freezes input boxes when viewing todo items and then trying to write new entry

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const todoData = require('../data/todo');
+let todoData = require('../data/todo');
 
 module.exports = (app) => {
     app.get("/api/notes", (req, res) => res.json(todoData));
@@ -8,6 +8,16 @@ module.exports = (app) => {
         let data = req.body;
         data.id = todoData.length + 1;
         todoData.push(data);
+        res.json(todoData);
+    });
+
+    app.delete("/api/notes/:id", (req,res) => {
+        const targetId = req.params.id;
+        for (i=0;i<todoData.length;i++) {
+            if (todoData[i].id == targetId) {
+                todoData.splice(i,1);
+            };
+        };
         res.json(todoData);
     });
 };
