53f0804a8f3b842784771a159e8e89e124c08f62
Oliver Shih <olivershih@gmail.com>
2021-04-15 23:45:28 -0700

    fixed minor issue with duplicate ids

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,25 @@
 const path = require('path');
-let todoData = require('../data/todo');
+let notesData = require('../data/notes');
+let id = 1;
 
 module.exports = (app) => {
-    app.get("/api/notes", (req, res) => res.json(todoData));
+    app.get("/api/notes", (req, res) => res.json(notesData));
 
     app.post("/api/notes", (req, res) => {
         let data = req.body;
-        data.id = todoData.length + 1;
-        todoData.push(data);
-        res.json(todoData);
+        data.id = id;
+        id += 1;
+        notesData.push(data);
+        res.json(notesData);
     });
 
     app.delete("/api/notes/:id", (req,res) => {
         const targetId = req.params.id;
-        for (i=0;i<todoData.length;i++) {
-            if (todoData[i].id == targetId) {
-                todoData.splice(i,1);
+        for (i=0;i<notesData.length;i++) {
+            if (notesData[i].id == targetId) {
+                notesData.splice(i,1);
             };
         };
-        res.json(todoData);
+        res.json(notesData);
     });
 };
